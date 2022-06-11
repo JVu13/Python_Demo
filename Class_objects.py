5e0ebd26d50ca518cdd2bc1b0ae84bbdf22d9012
John <vujohn58@gmail.com>
2022-06-11 00:11:01 -0400

    edit class_object

--- Class_objects.py
+++ Class_objects.py
@@ -7,6 +7,18 @@ class Robot:
     def introduce_self(self):
         print("My name is " + self.name) #this in Java
 
+class Person:
+    def __init__(self, n,p,i):
+        self.name = n
+        self.personality = p
+        self.is_siting = i
+
+    def sit_down(self):
+        self.is_siting = True
+
+    def stand_up(self):
+        self.is_siting = False
+
 # r1 = Robot()
 # r1.name = "Tome"
 # r1.color = "red"
@@ -23,5 +35,14 @@ class Robot:
 r1 = Robot("Tom","red",30)
 r2 = Robot("Jerry","blue",40)
 
-r1.introduce_self()
-r2.introduce_self()
+# r1.introduce_self()
+# r2.introduce_self()
+
+p1 = Person("Alice", "agressive", False)
+p2 = Person("Becky", "talkative", True)
+
+# p1 owns r2
+p1.robot_owned = r2
+p2.robot_owned = r1
+
+p1.robot_owned.introduce_self()
